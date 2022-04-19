993a10549b308e21435c383f01c22dddc6b653f7
Abtin Mazhari Ravesh (715453) <AMazhariRa@smail.pembinatrails.ca>
2022-04-19 13:52:40 -0500

    3

--- index.js
+++ index.js
@@ -27,3 +27,13 @@ const map = new mapboxgl.Map({
 });
 
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+ 
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
